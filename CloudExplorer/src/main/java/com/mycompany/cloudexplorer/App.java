b510556dde2a67486b109d41b5efcf9722cdba97
ntu-user <ntu-uer@ntu-user>
2022-12-13 21:27:32 +0000

    Initial Commit

---
+++ CloudExplorer/src/main/java/com/mycompany/cloudexplorer/App.java
@@ -0,0 +1,38 @@
+package com.mycompany.cloudexplorer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Stage secondaryStage = new Stage();
+        
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("primary.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root,640,480);
+            secondaryStage.setScene(scene);
+            secondaryStage.setTitle("Cloud Explorer");
+            secondaryStage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
